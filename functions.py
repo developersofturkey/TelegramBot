533a84f47c2965c1c11b13afec2e284ab817e8a6
mentalrob <beratozgur.yaman@ogr.deu.edu.tr>
2019-08-16 17:55:39 +0300

    functions.py created and main.py updated

---
+++ functions.py
@@ -0,0 +1,4 @@
+
+
+def only_group_filter(message):
+    return message.chat.type == "group"
--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import telebot # https://github.com/eternnoir/pyTelegramBotAPI
 import config # config.py
+from functions import only_group_filter
 
 bot = telebot.TeleBot(config.API_TOKEN)
 
-@bot.message_handler(commands=['start', 'help'])
+@bot.message_handler(func=only_group_filter ,commands=['start', 'help'])
 def send_welcome(message):
 	bot.reply_to(message, "Howdy, how are you doing?")
+
+
+bot.polling()
