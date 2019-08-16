ecdf220d78edc4be64b7c7d85e2e1f3791e31d56
mentalrob <beratozgur.yaman@ogr.deu.edu.tr>
2019-08-17 00:27:01 +0300

    send_hello_msg and send_leave_msg modules for node.js

--- src/modules/send_hello_msg.js
+++ src/modules/send_hello_msg.js
@@ -1,5 +1,6 @@
 export default (bot) => {
     bot.on('new_chat_members' , (ctx) => {
-        console.log(ctx.message);
+        var username = ctx.message.new_chat_member.first_name
+        ctx.replyWithHTML(`Hoşgeldin <b>${username}</b>, gruptaki diğer üyelere kendini kısaca tanıtır mısın ?`)
     })
 }
