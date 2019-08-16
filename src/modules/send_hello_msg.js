9ef51d73e8b64e73c298e99f779f5f8f2760a753
Halil İbrahim ceylan <hibrahim.ceylan@std.hku.edu.tr>
2019-08-17 01:23:24 +0300

    random messages for welcome message.

--- src/modules/send_hello_msg.js
+++ src/modules/send_hello_msg.js
@@ -1,7 +1,20 @@
 export default (bot) => {
-    bot.on('new_chat_members' , (ctx) => { // yeni bir üye gruba katıldığında
+    bot.on('new_chat_members', (ctx) => { // yeni bir üye gruba katıldığında
         var username = ctx.message.new_chat_member.first_name  // context'den kullanıcının adını alıyoruz
-        ctx.replyWithHTML(`Hoşgeldin <b>${username}</b>, gruptaki diğer üyelere kendini kısaca tanıtır mısın ?`) //HTML markup ile kullanıcıya hoşgeldin mesajı gönderiyoruz.
+        var welcome_messages_first = [
+            `Hoşgeldin <b>${username}</b>, `,
+            `Açılın! <b>${username}</b> geldi, rica etsem`,
+            `Kimler Kimler Gelmiş, Şeref Verdiniz <b>${username}</b>, bu arada`,
+        ]
+
+        var welcome_messages_second = [
+            'gruptaki diğer üyelere kendini kısaca tanıtır mısın ?',
+            'kendinden biraz bahseder misin ?',
+            'hangi konulara ilgi duyuyorsun bizimle paylaşır mısın ?',
+        ]
+
+        var message = welcome_messages_first[Math.floor(Math.random() * welcome_messages_first.length)] + ' ' + welcome_messages_second[Math.floor(Math.random() * welcome_messages_second.length)];
+        ctx.replyWithHTML(message) //HTML markup ile kullanıcıya hoşgeldin mesajı gönderiyoruz.
     })
 }
 // Event Type'lara https://telegraf.js.org/#/?id=update-types burdan ulaşabilirsiniz
