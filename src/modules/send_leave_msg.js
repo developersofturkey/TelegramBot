d3a71d90000f3cea48dc0e8dedf2c6bd0db82911
Halil İbrahim ceylan <hibrahim.ceylan@std.hku.edu.tr>
2019-08-18 20:30:44 +0300

    leave message delete.

--- src/modules/send_leave_msg.js
+++ src/modules/send_leave_msg.js
@@ -1,6 +1,5 @@
 export default (bot) => {
-    bot.on('left_chat_member' , (ctx) => {
-        var username = ctx.message.left_chat_member.first_name;
-        ctx.replyWithHTML(`Görüşürüz <b>${username}</b>, seni tanımak güzeldi !`)
+    bot.on('left_chat_member', (ctx) => {
+        ctx.deleteMessage();
     })
 }
